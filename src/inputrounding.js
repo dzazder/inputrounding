9f43c6228c290e2b2e40c2f7a463c197cec107cd
Bartosz Lampart <dzazder@gmail.com>
2017-04-11 19:02:03 +0200

    rounding action

--- src/inputrounding.js
+++ src/inputrounding.js
@@ -1,5 +1,18 @@
-$.fn.inputrounding = function() {
-    this.on("click", function() {
-        console.log("inputrounding")
-    } );
-};
+(function ($) {
+    $.fn.inputrounding = function (options) {
+        var opts = $.extend({}, $.fn.inputrounding.defaults, options);
+
+        for (var i = 0; i < opts.actions.length; i++) {
+            var action = opts.actions[i];
+
+            this.on(action, function () {
+                this.value = parseFloat(this.value).toFixed(opts.decimalPlaces);
+            });
+        };
+    }
+
+    $.fn.inputrounding.defaults = {
+        decimalPlaces: 2,
+        actions: ["blur"]
+    }
+}(jQuery));
